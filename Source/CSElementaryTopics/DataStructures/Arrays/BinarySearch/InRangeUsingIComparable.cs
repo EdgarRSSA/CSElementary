155751943aeaab653309e9fecc8bcd41dbf74e23
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-22 11:17:23 -0600

    Busqueda binaria usando Icomparable en un rango.
    Se busca un elemento en un rango, cada elemento debe implementar IComparable.

---
+++ Source/CSElementaryTopics/DataStructures/Arrays/BinarySearch/InRangeUsingIComparable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Elementary.DataStructures.Arrays.BinarySearch
+{
+	public static partial class Topic
+	{
+		[Test(Active = false)]
+		public static void InRangeUsingIComparable()
+		{
+			Console.WriteLine("Busqueda binaria en un Rango, usando IComparable.");
+
+			// ARRAY
+			Examples.Person[] personArray =
+			{
+				new Examples.Person("Mari1"),
+				new Examples.Person("Mari2"),
+				new Examples.Person("Mari3"),
+				new Examples.Person("Mari4"),
+				new Examples.Person("Maria"),
+				new Examples.Person("maria"),
+				new Examples.Person("Mxria"),
+				new Examples.Person("Mariat"),
+				new Examples.Person("Maria"),
+				new Examples.Person("Marib"),
+				new Examples.Person("Mariaa"),
+				new Examples.Person("Maria"),
+				new Examples.Person("mMaria"),
+				new Examples.Person("MMaria"),
+			};
+
+			// ORDENAR            
+			Array.Sort(personArray);
+			for ( int i = 0; i < personArray.Length; i++ )
+			{
+				Console.WriteLine($"\t{i}-{ personArray[i].Name}");
+			}
+
+			// BUSCAR EN RANGO
+			Examples.Person target = new Examples.Person("Maria");
+			int location = Array.BinarySearch(personArray, 0, 5, target);
+			Console.WriteLine($"Existe {target.Name} en el rango 0-5 : {( location < 0 ? "No" : ( "Si en " + location ) )}");
+		}
+
+
+	}
+}
