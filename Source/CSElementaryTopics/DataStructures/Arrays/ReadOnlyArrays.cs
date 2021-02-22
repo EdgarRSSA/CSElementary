5a355e9a24afce2c6663f61da9321033005c5c38
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-22 10:16:22 -0600

    Arrays de solo lectura.
    Se pruban este tipo de arrays.

---
+++ Source/CSElementaryTopics/DataStructures/Arrays/ReadOnlyArrays.cs
@@ -0,0 +1,31 @@
+﻿namespace Elementary.DataStructures.Arrays
+{
+	public static partial class Topics
+	{
+
+		[Test(Active = false)]
+		public static void ReadOnly()
+		{
+			System.Console.WriteLine("Arrays de solo lectura.");
+
+			// ARRAY PRINCIPAL
+			byte[] arr = new byte[100];
+
+			// MODIFICACIONES INICIALES
+			arr[7] = 87;
+
+			// READ ONLY ARRAY
+			var arrForRead = System.Array.AsReadOnly<byte>(arr);
+
+			// MODIFICACIONES DEL ARRAY PRINCIPAL
+			arr[45] = 127;
+			arr[97] = 200;
+
+			System.Console.WriteLine("Verificar modificaciones en solo lectura:");
+			System.Console.WriteLine("Contiene el valor 127:" + arrForRead.Contains(127));
+			System.Console.WriteLine("Contiene el valor 87:" + arrForRead.Contains(87));
+			System.Console.WriteLine("Contiene el valor 200:" + arrForRead.Contains(200));
+		}
+
+	}
+}
