5a7bafaa9628ea1909988a7f7d9b457c36df043e
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-22 09:17:58 -0600

    Se crea el Atributo de ejecucion de  metodos estaticos.
    El atributo TestAttribute sera aplicado a cada metodo estatico que se requiera
    ejecutar dentro del modulo del ensamblado.

---
+++ Source/CSElementaryConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+
+namespace Elementary
+{
+	internal partial class Program
+	{
+		/// <summary>
+		/// Console Start.
+		/// </summary>
+		/// <param name="args">Console arguments.</param>
+		private static void Main(string[] args)
+		{
+			Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+			Execute();
+		}
+
+		/// <summary>
+		/// Ejecucion de todos los metodos estaticos marcados como TestAttribute.
+		/// </summary>
+		private static void Execute()
+		{
+			Module module = Assembly.GetExecutingAssembly().GetModules()[0];
+			Type[] types = module.GetTypes();
+			foreach ( var type in types )
+			{
+				if ( type.IsClass )
+				{
+					MethodInfo[] methodsInfo = type.GetMethods();
+					foreach ( var method in methodsInfo )
+					{
+						if ( method.IsStatic )
+						{
+							string nameClass = type.Name;
+							string nameMethod = method.Name;							
+							bool isTesteable = method.GetCustomAttribute<TestAttribute>()!=null;
+							if ( isTesteable && method.GetCustomAttribute<TestAttribute>().Active )
+							{
+								Console.WriteLine($"\n\t{"RUN".PadRight(5)}{nameClass}.{nameMethod}:\n");
+								method.Invoke(null, null);
+							}
+							else
+							{
+								//Console.WriteLine($"{"-".PadRight(5)}{nameClass}.{nameMethod}");
+							}
+						}
+					}
+				}
+			}
+
+		}
+		
+	}
+}
+
---
+++ Source/CSElementaryConsole/TestAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Elementary
+{
+	/// <summary>
+	/// Ejecuta los metodos estaticos segun la propiedad Active.
+	/// </summary>
+	[System.AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+	sealed class TestAttribute : Attribute
+	{
+		/// <summary>
+		/// Determina si un Metodo Estatico se ejecutara.
+		/// </summary>
+		public bool Active { get; set; }
+		public TestAttribute()
+		{
+			Active = false;
+			// Este atributo idenfitica y ejecuta un Metodo Statico.
+		}
+	}
+}
+
