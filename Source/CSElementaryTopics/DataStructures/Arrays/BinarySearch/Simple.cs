0d53c145ff0fd6aa0cf1dfcfe8c4d3c3e3b25433
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-22 10:29:18 -0600

    Busqueda Binaria Basica.
    Se busca un elemento en un array ordenado.

---
+++ Source/CSElementaryTopics/DataStructures/Arrays/BinarySearch/Simple.cs
@@ -0,0 +1,22 @@
+﻿namespace Elementary.DataStructures.Arrays.BinarySearch
+{
+	public static partial class Topic
+	{
+		[Test(Active = false)]
+		public static void SimpleElement()
+		{
+			byte[] bytes = new byte[100];
+			bytes[25] = 55;
+
+			// ORDENAR
+			System.Array.Sort<byte>(bytes);
+
+			byte target = 55;
+			var location = System.Array.BinarySearch(bytes, target);
+
+			System.Console.WriteLine($"Busqueda Binaria de '55' encontrado en {location}");
+			System.Console.WriteLine($"55 = {bytes[location]}");
+		}
+
+	}
+}
