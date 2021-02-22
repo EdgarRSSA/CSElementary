de045ebb2f19608fc19fda8fe393c905a692d892
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-22 09:53:29 -0600

    Renombre del proyecto.

--- Source/CSElementaryTopics/Properties/AssemblyInfo.cs
+++ Source/CSElementaryTopics/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSElementaryConsole")]
+[assembly: AssemblyTitle("CSElementaryTopics")]
 [assembly: AssemblyDescription("C# Elementary Language Topics")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EdgarRSSA/CSElementary")]
-[assembly: AssemblyProduct("CSElementaryConsole")]
+[assembly: AssemblyProduct("CSElementaryTopics")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("EdgarRSSA/CSElementary")]
 [assembly: AssemblyCulture("")]
