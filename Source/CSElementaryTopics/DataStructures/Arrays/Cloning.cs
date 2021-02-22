b213b7c2f1afdccd0d4ec3e175eabca70a7d8fd3
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-22 13:36:43 -0600

    Clonando un Array.

---
+++ Source/CSElementaryTopics/DataStructures/Arrays/Cloning.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Elementary.DataStructures.Arrays
+{
+
+	public static partial class Topics
+	{
+		[Test(Active = false)]
+		public static void Clone()
+		{
+			// Array Principal
+			string[] mainArray = {
+				"Alfa","Beta","Gamma"
+			};
+			Console.WriteLine("Lista de elementos clonados:");
+			for ( int i = 0; i < mainArray.Length; i++ )
+			{
+				Console.WriteLine($"\t{i}-{mainArray[i]}");
+			}
+
+			// Clonando un array
+			var clone = (object[])mainArray.Clone();
+
+			mainArray[0] = "xxx"; // Modificacion sin efecto en el clon.
+
+			Console.WriteLine("Lista de elementos clonados:");
+			for ( int i = 0; i < clone.Length; i++ )
+			{
+				Console.WriteLine($"\t{i}-{clone[i]}");
+			}
+
+		}
+
+
+	}
+
+
+}
