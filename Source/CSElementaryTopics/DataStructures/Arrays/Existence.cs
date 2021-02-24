0b7cba5fd601bb39fa2d6d8481a681db38fc4582
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-24 00:51:42 -0600

    Existencia de un elemento en un array.

---
+++ Source/CSElementaryTopics/DataStructures/Arrays/Existence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Elementary.DataStructures.Arrays
+{
+	public static partial class Topics
+	{
+
+		[Test(Active = false)]
+		public static void ElementExistece()
+		{
+			Console.WriteLine("Existencia de un elemento en un array:");
+
+			int[] elements =
+			{
+				1,2,3,4,5,6,7
+			};
+
+			int target = 8;
+
+			bool exist = Array.Exists<int>(elements, (match) =>
+			{
+				return match == target;
+			});
+
+			Console.WriteLine($"Existencia de {target} en {elements.GetType().Name} -> {exist}");
+
+
+		}
+
+	}
+
+}
