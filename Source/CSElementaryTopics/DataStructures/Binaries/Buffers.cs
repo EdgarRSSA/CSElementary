ac3c82929e7b13b338fd6644783eb23e03f0b690
EdgarRS@ <edgar_rrr@outlook.com>
2021-02-28 12:35:15 -0600

    Manejo de bytes de tipos primitivos.
    Uso de BitConverter,
    Uso de Buffer,
    Uso de Convert<T,T>

---
+++ Source/CSElementaryTopics/DataStructures/Binaries/Buffers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Elementary.DataStructures.Binaries
+{
+	public static partial class Buffers
+	{
+
+		[Test(Active = false)]
+		public static void Use()
+		{
+			Console.WriteLine("Using C#/Buffers");
+
+			/**
+			 * Buffer is applicable to the following primitive types: Boolean, Char, SByte, 
+			 * Byte, Int16, UInt16, Int32, UInt32, Int64, UInt64, IntPtr, UIntPtr, 
+			 * Single, and Double.
+			 * 
+			 * 
+			 */
+
+			// Primitive Arrays
+			char[] myarray = "asdcvbdfg".ToCharArray();
+
+
+
+			/**
+			 * Copies a specified number of bytes from a source array starting 
+			 * at a particular offset to a destination array starting at a 
+			 * particular offset.
+			 * 
+			 */
+			Array destino = Array.CreateInstance(typeof (char),15);
+			Buffer.BlockCopy(myarray, 0, destino, 0, myarray.Length);
+
+			Console.WriteLine($"\nBlockCopy Count: {destino.Length}");
+			foreach ( var item in destino )
+			{
+				Console.WriteLine($"\t-{item}");
+			}
+
+
+			/**
+			 * Returns the number of bytes in the specified array.
+			 * 
+			 */
+			Console.WriteLine($"\nByteLenght: {Buffer.ByteLength(destino)}");
+
+
+			/**
+			 * Set and Get Bytes.
+			 *
+			 */
+			Buffer.SetByte(destino, 0, 65);
+			Console.WriteLine($"\nSetByte 45 in Buffer: {(char) Buffer.GetByte(destino, 0)}");
+
+
+
+
+		}
+
+	}
+}
---
+++ Source/CSElementaryTopics/DataStructures/Binaries/ByteConverters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Elementary.DataStructures.Binaries
+{
+	public static partial class ByteConverters
+	{
+		[Test(Active = false)]
+		public static void Use()
+		{
+			/**
+			 * BitConverters
+			 * 
+			 */
+			Console.WriteLine("\nConvertidor de tipos primitivos a bytes");
+
+			// Enteros
+			int[]  intss = {1,288,30000,444444444,500,6};
+
+			// Convertidor a bytes.			
+			Converter<int,byte[]> converter1 = (val)=>
+			{
+				return BitConverter.GetBytes(val);
+			};
+			byte[][] bytessb = Array.ConvertAll<int, byte[]>(intss, converter1);
+
+			// Imprimir listas de bytes.		
+			foreach ( Array item in bytessb )
+			{
+				Console.Write($"\t{BitConverter.ToInt32((byte[]) item, 0),-15}:\t");
+				foreach ( var item2 in item )
+				{
+					Console.Write($"{item2,-4}");
+				}
+				Console.WriteLine();
+			}
+		}
+
+
+	}
+}
